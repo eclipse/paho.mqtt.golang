eb3d6fc6078ca386db382bb62be4bef7d590fb3c
Isaac Seymour <i.seymour@oxon.org>
2016-03-29 16:02:30 +0100

    Remove unused MessageID assignment

--- net.go
+++ net.go
@@ -109,10 +109,6 @@ func outgoing(c *client) {
 			return
 		case pub := <-c.obound:
 			msg := pub.p.(*packets.PublishPacket)
-			if msg.Qos != 0 && msg.MessageID == 0 {
-				msg.MessageID = c.getID(pub.t)
-				pub.t.(*PublishToken).messageID = msg.MessageID
-			}
 
 			if c.options.WriteTimeout > 0 {
 				c.conn.SetWriteDeadline(time.Now().Add(c.options.WriteTimeout))
