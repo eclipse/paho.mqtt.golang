22c9f640ea9b19a734a2598a3f9323b597f8c1ed
Orne Brocaar <info@brocaar.com>
2016-04-03 11:22:03 +0200

    Make FVTAddr configurable by using env variable.
    Signed-off-by: Orne Brocaar <info@brocaar.com>

--- fvt_test.go
+++ fvt_test.go
@@ -14,13 +14,23 @@
 
 package mqtt
 
+import "os"
+
 // Use setup_IMA.sh for IBM's MessageSight
 // Use fvt/rsmb.cfg for IBM's Really Small Message Broker
 // Use fvt/mosquitto.cfg for the open source Mosquitto project
 
-// Set these values to the URI of your MQTT Broker before running go-test
-const (
-	FVTAddr = "iot.eclipse.org"
-	FVTTCP  = "tcp://" + FVTAddr + ":1883"
-	FVTSSL  = "ssl://" + FVTAddr + ":8883"
+var (
+	FVTAddr string
+	FVTTCP  string
+	FVTSSL  string
 )
+
+func init() {
+	FVTAddr := os.Getenv("TEST_FVT_ADDR")
+	if FVTAddr == "" {
+		FVTAddr = "iot.eclipse.org"
+	}
+	FVTTCP = "tcp://" + FVTAddr + ":1883"
+	FVTSSL = "ssl://" + FVTAddr + ":8883"
+}
