fc07c1a40dcfc2be189d19eae9db3c763943c099
Daniel Beseda <daniel.beseda@energomonitor.cz>
2021-04-28 11:59:36 +0000

    Redact password in Stringer of packets.ConnectPacket

--- packets/connect.go
+++ packets/connect.go
@@ -29,7 +29,11 @@ type ConnectPacket struct {
 }
 
 func (c *ConnectPacket) String() string {
-	return fmt.Sprintf("%s protocolversion: %d protocolname: %s cleansession: %t willflag: %t WillQos: %d WillRetain: %t Usernameflag: %t Passwordflag: %t keepalive: %d clientId: %s willtopic: %s willmessage: %s Username: %s Password: %s", c.FixedHeader, c.ProtocolVersion, c.ProtocolName, c.CleanSession, c.WillFlag, c.WillQos, c.WillRetain, c.UsernameFlag, c.PasswordFlag, c.Keepalive, c.ClientIdentifier, c.WillTopic, c.WillMessage, c.Username, c.Password)
+	var password string
+	if len(c.Password) > 0 {
+		password = "<redacted>"
+	}
+	return fmt.Sprintf("%s protocolversion: %d protocolname: %s cleansession: %t willflag: %t WillQos: %d WillRetain: %t Usernameflag: %t Passwordflag: %t keepalive: %d clientId: %s willtopic: %s willmessage: %s Username: %s Password: %s", c.FixedHeader, c.ProtocolVersion, c.ProtocolName, c.CleanSession, c.WillFlag, c.WillQos, c.WillRetain, c.UsernameFlag, c.PasswordFlag, c.Keepalive, c.ClientIdentifier, c.WillTopic, c.WillMessage, c.Username, password)
 }
 
 func (c *ConnectPacket) Write(w io.Writer) error {
