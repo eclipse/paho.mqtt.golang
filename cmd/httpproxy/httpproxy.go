fecda221af1e041fedc880074540958a1a8b05c1
Matt Brittan <matt@brittan.nz>
2021-07-21 09:34:07 +1200

    Add license headers to all files and move from EPL v1.0 to EPL v2.0 (see faq - https://www.eclipse.org/legal/epl-2.0/faq.php). Note that project is dual licensed - EPL v2.0 and EDL v1.0.

--- cmd/httpproxy/httpproxy.go
+++ cmd/httpproxy/httpproxy.go
@@ -1,13 +1,23 @@
+/*
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    https://www.eclipse.org/legal/epl-2.0/
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"golang.org/x/net/proxy"
 	"net"
 	"net/http"
 	"net/url"
-
-	"golang.org/x/net/proxy"
 )
 
 // httpProxy is a HTTP/HTTPS connect capable proxy.
--- websocket.go
+++ websocket.go
@@ -1,3 +1,16 @@
+/*
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    https://www.eclipse.org/legal/epl-2.0/
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ */
+
 package mqtt
 
 import (
