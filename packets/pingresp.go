86ecf324b6711eb97c175d8327bfbac7f229f5da
Matt Brittan <matt@brittan.nz>
2021-07-21 10:05:14 +1200

    Update copyright year and add "and others"

--- packets/pingresp.go
+++ packets/pingresp.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 IBM Corp.
+ * Copyright (c) 2021 IBM Corp and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
