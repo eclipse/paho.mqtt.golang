fc1698ad5e28e1e6b36581565d086614fd2244af
Gil Adda <gil.adda@hotmail.com>
2021-12-22 13:50:46 +0200

    Add client test for nil function

--- options.go
+++ options.go
@@ -441,6 +441,8 @@ func (o *ClientOptions) SetDialer(dialer *net.Dialer) *ClientOptions {
 // The passed in function should return an open `net.Conn` or an error (see the existing openConnection function for an example)
 // It enables custom networking types in addition to the defaults (tcp, tls, websockets...)
 func (o *ClientOptions) SetCustomOpenConectionFn(customOpenConnectionfn OpenConnectionFunc) *ClientOptions {
-	o.CustomOpenConnectionFn = customOpenConnectionfn
+	if customOpenConnectionfn != nil {
+		o.CustomOpenConnectionFn = customOpenConnectionfn
+	}
 	return o
 }
