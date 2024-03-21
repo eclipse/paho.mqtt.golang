d0120ee4840da1a8b7019ed65aa80ab69ff32c87
Daniele Vasselli <daniele.vasselli@northvolt.com>
2024-03-21 15:44:27 +0100

    Replace the time.After with the timer for efficiency.
    Signed-off-by: Daniele Vasselli <vasselli.daniele@gmail.com>

--- client.go
+++ client.go
@@ -799,9 +799,15 @@ func (c *client) Publish(topic string, qos byte, retained bool, payload interfac
 		if publishWaitTimeout == 0 {
 			publishWaitTimeout = time.Second * 30
 		}
+		t := time.NewTimer(publishWaitTimeout)
+		defer func() {
+			if !t.Stop() {
+				<-t.C
+			}
+		}()
 		select {
 		case c.obound <- &PacketAndToken{p: pub, t: token}:
-		case <-time.After(publishWaitTimeout):
+		case <-t.C:
 			token.setError(errors.New("publish was broken by timeout"))
 		}
 	}
